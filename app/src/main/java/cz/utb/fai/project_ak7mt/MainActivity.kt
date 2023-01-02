94de59b34b229975760831b70bfdebcc96133558
Iniesta <gtaczgamesplay@gmail.com>
2023-01-02 17:52:50 +0100

    úprava textu

--- app/src/main/java/cz/utb/fai/project_ak7mt/MainActivity.kt
+++ app/src/main/java/cz/utb/fai/project_ak7mt/MainActivity.kt
@@ -3,7 +3,10 @@ package cz.utb.fai.project_ak7mt
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()
+{
+    private
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
