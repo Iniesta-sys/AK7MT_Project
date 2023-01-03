892a7b9af19bc019c701dee2ffe363e991b03c89
Iniesta <gtaczgamesplay@gmail.com>
2023-01-03 12:06:17 +0100

    first test

---
+++ app/src/main/java/cz/utb/fai/project_ak7mt_v2/Currency.kt
@@ -0,0 +1,8 @@
+package cz.utb.fai.project_ak7mt_v2
+
+class Currency(
+    var USD: Double,
+    var EUR: Double,
+    var CZK: Double
+
+)
---
+++ app/src/main/java/cz/utb/fai/project_ak7mt_v2/Request.kt
@@ -0,0 +1,10 @@
+package cz.utb.fai.project_ak7mt_v2
+
+class Request(
+   var result:String,
+   var time_last_update_utc: String,
+   var time_next_update_utc:String,
+   var base_code:String,
+   var rates: Currency
+
+)
