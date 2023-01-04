ba7219bf03c91617f6984f1ea859a3b562bafa66
Iniesta <gtaczgamesplay@gmail.com>
2023-01-04 20:20:15 +0100

    Main page and Second page

---
+++ app/src/main/java/cz/utb/fai/project_ak7mt_v2/MainCurrency.kt
@@ -0,0 +1,64 @@
+package cz.utb.fai.project_ak7mt_v2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.gson.Gson
+import cz.utb.fai.project_ak7mt_v2.databinding.ActivityMainBinding
+import cz.utb.fai.project_ak7mt_v2.databinding.ActivityMainCurrencyBinding
+import java.io.InputStreamReader
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
+
+
+class MainActivityCurrency : AppCompatActivity()
+{
+    private lateinit var binding: ActivityMainCurrencyBinding
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainCurrencyBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        fetchCurrencyData().start()
+    }
+
+
+    private fun fetchCurrencyData(): Thread
+    {
+        return Thread {
+            val url = URL("https://open.er-api.com/v6/latest/czk")
+            val connection = url.openConnection() as HttpsURLConnection
+
+            if(connection.responseCode == 200)
+
+            {
+                val inputSystem = connection.inputStream
+                val inputStreamReader = InputStreamReader(inputSystem, "UTF-8")
+                val request = Gson().fromJson(inputStreamReader, Request::class.java)
+                updateUI(request)
+                inputStreamReader.close()
+                inputSystem.close()
+            }
+            else
+            {
+                binding.baseCurrency.text = "Připojení se nezdařilo"
+
+            }
+        }
+
+    }
+
+    private fun updateUI(request: Request)
+    {
+        runOnUiThread{
+            kotlin.run {
+                binding.lastUpdate.text = request.time_last_update_utc
+                binding.czk.text = String.format("CZK:  %.2f", request.rates.CZK)
+                binding.eur.text = String.format("EUR:  %.2f", request.rates.EUR)
+                binding.usd.text = String.format("USD:  %.2f", request.rates.USD)
+            }
+
+        }
+    }
+}
